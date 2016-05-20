5ca6549b25f4d66a966c5e4878574e3cd0012c1e
kirill <apuc06@mail.ru>
2016-05-20 12:58:00 +0300

    index

--- index.php
+++ index.php
@@ -29,8 +29,8 @@
                               
     <section class="entry">
     	<div class="entry__text">
-    		<h1>Владимир Гойдин</h1>
-    		<h3>Земля, недвижимость, правовое сопровождение</h3>
+    		<h1>Павел Раков</h1>
+    		<h3>Самый известный и эффективный тренер женских программ в России</h3>
     		<p>
                 <?php
                     $text = get_post( 54 );
@@ -45,17 +45,122 @@
         <div class="aboutme__text">
             <div class="aboutme__text--line"></div>
             <h3>Обо мне</h3>
-            <?= do_shortcode('[about_me_text]');?>
+            <p>
+                Павел Раков - эксперт и телеведущий во многих средствах массовой информации.
+            </p>
+            <p>
+                Его мероприятия посещают более 50 тысяч человек в год, и еще более 500 тысяч ежегодно обучаются онлайн;
+            </p>
+            <p>
+                В 2015 году удостоен профессиональной докторской степени (звания профессора психологии)
+                Международным академическим аккредитационным и аттестационным комитетом (МАААК);
+            </p>
+            <p>
+                Является признанным экспертом в отношениях между мужчиной и женщиной и желанным
+                гостем на российском телевидении и радио: "Пусть говорят" (Первый канал),
+                "Прямой эфир" (Россия-1), телеканалы НТВ, Домашний, Москва 24, радиостанции Маяк, Радио 7, Романтика, Фреш;
+            </p>
+            <p>
+                Автор серии книг: «Где взять силы для успеха», «На самом деле я умная, но живу как дура»,
+                «Дура с двумя высшими желает познакомиться», которые стали бестселлерами в России и СНГ;
+            </p>
+            <p>
+                Ведет рубрику в журнале Cosmopolitan.
+            </p>
         </div>
-        <?= do_shortcode('[about_me]');?>
+        <div class="aboutme__photo">
+            <div class="aboutme__photo--front">
+                <!--<img src="'.$slides[0]->small_img.'" alt="">-->
+            </div>
+            <div class="aboutme__photo--back">
+                <img src="<?php bloginfo('template_directory'); ?>/img/002.jpg" alt="">
+            </div>
+        </div>
     </section>
    
     <section class="services">
       <a id="go_serv"></a>
         <div class="services__arrow"></div>
-        <h1 class="block_title">Услуги</h1>
+        <h1 class="block_title">БЛИЖАЙШИЕ МЕРОПРИЯТИЯ</h1>
         <div class="fotorama" data-minwidth="100%" data-maxheight="760" data-fit="scaledown" data-nav="false" data-autoplay="true" >
-            <?= do_shortcode('[service]');?>
+            <div class="services__container">
+                <div class="services__container__col">
+                    <div class="services__img">
+                        <img src="<?php bloginfo('template_directory'); ?>/img/slide1.jpg" alt="">
+                    </div>
+                    <div class="services__text">
+                        <div class="services__text--box">
+                            <h4>Это прекрасная возможность создать или улучшить </h4>
+                            <p>отношения, о которых вы мечтали за очень короткий
+                                период времени!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="services__container__col">
+                    <div class="services__text">
+                        <div class="services__text--box">
+                            <h4>Скандально известная авторская встреча для женщин</h4>
+                            <p>28-29 мая в Санкт-Петербурге</p>
+                        </div>
+                    </div>
+                    <div class="services__img">
+                        <img src="<?php bloginfo('template_directory'); ?>/img/slide2.jpg" alt="">
+                    </div>
+                </div>
+                <div class="services__container__col">
+                    <div class="services__img">
+                        <img src="<?php bloginfo('template_directory'); ?>/img/slide4.jpg" alt="">
+                    </div>
+                    <div class="services__text">
+                        <div class="services__text--box">
+                            <h4>Мы этого добились! </h4>
+                            <p>Наше мероприятие стало лидером рынка, который собирает многотысячные залы, нас приглашают в разные города и страны.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="services__container">
+                <div class="services__container__col">
+                    <div class="services__img">
+                        <img src="<?php bloginfo('template_directory'); ?>/img/slide5.jpg" alt="">
+                    </div>
+                    <div class="services__text">
+                        <div class="services__text--box">
+                            <h4>Что ты получишь? </h4>
+                            <p>1) С тобой начнут знакомиться успешные мужчины.</p>
+                            <p>2) Твой мужчина, с которым ты живешь, станет ещё более успешным.</p>
+                            <p>3) Твой сын начнет лучше учиться и стремиться к развитию.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="services__container__col">
+                    <div class="services__text">
+                        <div class="services__text--box">
+                            <h4>Как стать «Девушкой на миллион»</h4>
+                            <p>09 - 10 июля 2016 г., Москва</p>
+                        </div>
+                    </div>
+                    <div class="services__img">
+                        <img src="<?php bloginfo('template_directory'); ?>/img/slide7.jpg" alt="">
+                    </div>
+                </div>
+                <div class="services__container__col">
+                    <div class="services__img">
+                        <img src="<?php bloginfo('template_directory'); ?>/img/slide6.jpg" alt="">
+                    </div>
+                    <div class="services__text">
+                        <div class="services__text--box">
+                            <h4>ЕГО МЕРОПРИЯТИЯ СТАЛИ ЛЕГЕНДОЙ.
+                                ЕГО ИМЯ ЗНАЮТ ВСЕ.
+                                ЕГО СЛУШАЮТ ДЕСЯТКИ ТЫСЯЧ ЖЕНЩИН. </h4>
+
+                            <p>ВЧЕРАШНИЕ "УЧИЛКИ" ВЫХОДЯТ ЗАМУЖ ЗА БИЗНЕСМЕНОВ, "СЕРЫЕ МЫШКИ" КАТАЮТСЯ НА BENTLEY,</p>
+                            <p>"ГОЛУБЫЕ ЧУЛКИ" СВЕРКАЮТ БРИЛЛИАНТАМИ.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 
